3b470856f29f793aaccc23022845bbaa3cdfb6c0
timolansberry <timothy3097@hotmail.com>
2025-10-05 12:37:28 -0400

    added firebase

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+// This file contains your actual Firebase API keys
+// DO NOT commit this file to version control - it's in .gitignore
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDXGvAhqZJ1fFdZGYvBQ_GinfQ_p_dkpdE",
+    authDomain: "portfolio-projects-1d337.firebaseapp.com",
+    databaseURL: "https://portfolio-projects-1d337-default-rtdb.firebaseio.com",
+    projectId: "portfolio-projects-1d337",
+    storageBucket: "portfolio-projects-1d337.firebasestorage.app",
+    messagingSenderId: "848327381159",
+    appId: "1:848327381159:web:55e560f81ecc6e6cd38b82",
+    measurementId: "G-6DBH9RN8TT"
+};
